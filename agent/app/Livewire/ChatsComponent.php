18c2fc8f1046f2133c65df9e150869620cf18dbe
Marien Mupenda <marien.mupenda@gmail.com>
2023-09-17 17:52:05 +0200

    add custommer chat page

---
+++ agent/app/Livewire/ChatsComponent.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Livewire;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Http;
+use Livewire\Component;
+
+class ChatsComponent extends Component
+{
+    public bool $newChat = false;
+    /**
+     * @var null
+     */
+    public ?array $chat;
+    public int $httpStatus;
+    public ?string $httpMessage;
+    public array $data;
+
+    public function mount(): void
+    {
+        $this->newChat = false;
+        $this->chat = null;
+    }
+
+    #[On('echo:chats,ChatEvent')]
+    public function notifyNewChat(): void
+    {
+        $this->newChat = true;
+    }
+
+    public function render(): Factory|\Illuminate\Foundation\Application|View|Application
+    {
+        return view('livewire.chats-component');
+    }
+
+    public function sendMessage(): void
+    {
+        $response = Http::acceptJson()
+            ->withHeaders([
+                'Authorization' => 'Bearer ' . env('AUTH_TOKEN'),
+            ])
+            ->asJson()
+            ->post('http://ensurance-ms.test/api/app/chats/create', [
+                'message' => 'Hello, I need help',
+            ]);
+
+        $this->httpStatus = $response->status();
+        $this->httpMessage = $response->body();
+
+        if ($response->successful()) {
+            $this->data = $response->json()['data'];
+        }
+    }
+}
---
+++ agent/resources/views/livewire/chats-component.blade.php
@@ -0,0 +1,26 @@
+<div class="p-3">
+    <h1>Customer Chat</h1>
+    <ul class="list-group">
+        @foreach($chat->messages??[] as $message)
+            <li class="list-group-item">
+                <div class="d-flex justify-content-between">
+                    <div>
+                        <h5>{{ $message->user->name }}</h5>
+                        <p>{{ $message->message }}</p>
+                    </div>
+                    <div>
+                        <small>{{ $message->created_at->diffForHumans() }}</small>
+                    </div>
+                </div>
+        @endforeach
+    </ul>
+    <div class="mt-3">
+        <input type="text" wire:model="message" class="form-control mb-3 mt-3" placeholder="Type your message here...">
+        <button wire:click="sendMessage" class="btn btn-primary">Send</button>
+    </div>
+    <div>
+        Status : {{ $httpStatus  }}<br>
+        Message: {{ $httpMessage }}<br>
+        Data: <pre>{{ json_encode($data, JSON_PRETTY_PRINT) }}</pre>
+    </div>
+</div>
